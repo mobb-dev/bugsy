3340863bce3e74b545fa65a4bbc7a324d97a4cfd
Kirill Efimov <kirill@mobb.dev>
2024-01-21 15:15:35 +0000

    version 0.0.72

--- src/features/analysis/scm/constants.ts
+++ src/features/analysis/scm/constants.ts
@@ -1,2 +1,2 @@
-export const MOBB_ICON_IMG = 'https://app.mobb.ai/mobb.png'
+export const MOBB_ICON_IMG = 'https://svgshare.com/i/12DK.svg'
 export const COMMIT_FIX_SVG = `https://app.mobb.ai/gh-action/commit-button.svg`

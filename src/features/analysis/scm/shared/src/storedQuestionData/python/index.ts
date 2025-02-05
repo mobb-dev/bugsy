b7bce256cfe0fdbf1afe5da0f268e736739812a5
Kirill Efimov <kirill@mobb.dev>
2025-02-05 10:50:12 +0000

    version 1.0.29

--- src/features/analysis/scm/shared/src/storedQuestionData/python/index.ts
+++ src/features/analysis/scm/shared/src/storedQuestionData/python/index.ts
@@ -2,11 +2,13 @@ import { IssueType_Enum } from '../../../../generates/client_generates'
 import { csrf } from './csrf'
 import { logForging } from './logForging'
 import { openRedirect } from './openRedirect'
+import { uncheckedLoopCondition } from './uncheckedLoopCondition'
 
 const vulnerabilities = {
   [IssueType_Enum.Csrf as string]: csrf,
   [IssueType_Enum.LogForging as string]: logForging,
-  [IssueType_Enum.LogForging as string]: openRedirect,
+  [IssueType_Enum.OpenRedirect as string]: openRedirect,
+  [IssueType_Enum.UncheckedLoopCondition as string]: uncheckedLoopCondition,
 }
 
 export default vulnerabilities

6242c3b288038a3d5e0e9f05c8e7d4c72d2296f8
Kirill Efimov <kirill@mobb.dev>
2025-02-28 09:37:14 +0000

    version 1.0.44

---
+++ src/features/analysis/scm/shared/src/storedQuestionData/python/systemInformationLeak.ts
@@ -0,0 +1,17 @@
+export const systemInformationLeak = {
+  errorMessage: {
+    content: () => 'Enter the error message that you want to use',
+    description: () => '',
+    guidance: () => '',
+  },
+  logMessage: {
+    content: () => 'Enter the message that you want to appear in the log',
+    description: () => '',
+    guidance: () => '',
+  },
+  add_log_message: {
+    content: () => 'Do you want to add a log message?',
+    description: () => '',
+    guidance: () => '',
+  },
+}

9984aaf96c883515225c8f2de03362987171b527
Kirill Efimov <kirill@mobb.dev>
2025-07-04 12:04:19 +0000

    version 1.0.110

--- src/features/analysis/scm/shared/src/storedQuestionData/js/openRedirect.ts
+++ src/features/analysis/scm/shared/src/storedQuestionData/js/openRedirect.ts
@@ -4,13 +4,21 @@ export const openRedirect = {
     description: () => '',
     guidance: () => '',
   },
-  allowlist: {
-    content: () => 'Allowed domains/paths',
-    description: () => description,
+  domainAllowlist: {
+    content: () => 'Allowed domains names',
+    description: () =>
+      'please provide a coma separated list of allowed domains names (example.com, example.org, etc.)',
+    guidance: () => '',
+  },
+  pathAllowlist: {
+    content: () => 'Allowed paths (URIs)',
+    description: () =>
+      'please provide a coma separated list of allowed path (/health, /api/v1/health, etc.)',
+    guidance: () => '',
+  },
+  includeProtocolValidation: {
+    content: () => 'Should HTTP or HTTPS protocol be enforced?',
+    description: () => 'please indicate if the protocol should be enforced',
     guidance: () => '',
   },
 }
-
-const description = `- *If external*, provide a coma separated list of allowed domains. \
-\n&nbsp;
-- *If internal*, provide a coma seperated list of allowed paths`
